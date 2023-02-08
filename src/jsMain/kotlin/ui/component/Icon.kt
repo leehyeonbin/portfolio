03480d5e0421f0141d212e75508557c9c9de4391
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-02-09 01:42:14 +0900

    ️️ ⚰️ :: 너무 조잡해보여서 아이콘 둘 다 관짝행

--- src/jsMain/kotlin/ui/component/Icon.kt
+++ src/jsMain/kotlin/ui/component/Icon.kt
@@ -7,96 +7,10 @@ import org.jetbrains.compose.web.dom.Div
 
 @Composable
 fun WifiIcon() {
-    Div(attrs = {
-        style {
-            width(14.px)
-            height(6.px)
-            borderRadius(8.px, 8.px, 0.px, 0.px)
-            borderTop(1.px, LineStyle.Solid, white)
-        }
-    }) {
-        Div(attrs = {
-            style {
-                width(10.px)
-                height(6.px)
-                borderRadius(6.px, 6.px, 0.px, 0.px)
-                borderTop(1.px, LineStyle.Solid, white)
-                marginTop(2.px)
-                marginLeft(2.px)
-            }
-        }) {
-            Div(attrs = {
-                style {
-                    width(6.px)
-                    height(6.px)
-                    borderRadius(6.px, 6.px, 0.px, 0.px)
-                    borderTop(1.px, LineStyle.Solid, white)
-                    marginTop(2.px)
-                    marginLeft(2.px)
-                }
-            }) {
-                Div(attrs = {
-                    style {
-                        width(1.px)
-                        height(1.px)
-                        backgroundColor(white)
-                        borderRadius(50.percent)
-                        marginTop(2.px)
-                        marginLeft(2.px)
-                    }
-                }) {
-
-                }
-            }
-        }
-    }
 
 }
 
 @Composable
 fun FrequencyIcon() {
-    Div (attrs = {
-        style {
-            marginLeft(15.px)
-        }
-    }){
-        Div(attrs = {
-            style {
-                width(1.px)
-                height(2.px)
-                backgroundColor(white)
-                position(Position.Absolute)
-            }
-        })
-        Div(attrs = {
-            style {
-                width(1.px)
-                height(4.px)
-                marginLeft(2.px)
-                marginTop((-2).px)
-                backgroundColor(white)
-                position(Position.Absolute)
-            }
-        })
-        Div(attrs = {
-            style {
-                width(1.px)
-                height(7.px)
-                marginLeft(4.px)
-                marginTop((-5).px)
-                backgroundColor(white)
-                position(Position.Absolute)
-            }
-        })
-        Div(attrs = {
-            style {
-                width(1.px)
-                height(10.px)
-                marginLeft(6.px)
-                marginTop((-8).px)
-                backgroundColor(white)
-                position(Position.Absolute)
-            }
-        })
-    }
+
 }
