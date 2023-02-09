1b444a72a153931b7205ec38126e6a318e2110d9
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-02-10 02:15:19 +0900

    ️🗂 :: 폴더 생성

--- src/jsMain/kotlin/screen/phone/device/S23.kt
+++ src/jsMain/kotlin/screen/phone/device/S23.kt
@@ -9,10 +9,9 @@ import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.css.Color.black
-import org.jetbrains.compose.web.css.Color.blue
 import org.jetbrains.compose.web.dom.Div
-import screen.phone.Phone
 import screen.phone.device.feature.Clock
+import screen.phone.display.DisplayScreen
 import ui.component.*
 
 
@@ -68,38 +67,7 @@ fun S23() {
                         }
                     }
                 }) {
-                    Div(attrs = {
-                        style {
-                            borderRadius(37.px)
-                            width(100.percent - 4.px)
-                            height(100.percent - 4.px)
-                            border {
-                                width = 2.px
-                                color = rgb(r = 74, g = 75, b = 77)
-                                style = LineStyle.Solid
-                            }
-                        }
-                    }) {
-                        val backgroundImage =
-                            "https://r1.community.samsung.com/t5/image/serverpage/image-id/6154775i034DD4C88767F1B5/image-dimensions/2500?v=v2&px=-1"
-                        Div(attrs = {
-                            style {
-                                marginLeft(2.px)
-                                marginTop(2.px)
-                                borderRadius(40.px)
-                                width(100.percent - 4.px)
-                                height(100.percent - 4.px)
-                                backgroundImage("url($backgroundImage)")
-                                backgroundPosition("center")
-                                backgroundSize("cover")
-                                position(Position.Relative)
-                                textAlign("center")
-                            }
-                        }) {
-                            StatusBar()
-                            Camera()
-                        }
-                    }
+                    DisplayScreen()
                 }
             }
         }
---
+++ src/jsMain/kotlin/screen/phone/display/DisplayScreen.kt
@@ -0,0 +1,43 @@
+package screen.phone.display
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.Div
+import screen.phone.device.StatusBar
+import ui.component.Camera
+
+@Composable
+fun DisplayScreen() {
+    Div(attrs = {
+        style {
+            borderRadius(37.px)
+            width(100.percent - 4.px)
+            height(100.percent - 4.px)
+            border {
+                width = 2.px
+                color = rgb(r = 74, g = 75, b = 77)
+                style = LineStyle.Solid
+            }
+        }
+    }) {
+        val backgroundImage =
+            "https://r1.community.samsung.com/t5/image/serverpage/image-id/6154775i034DD4C88767F1B5/image-dimensions/2500?v=v2&px=-1"
+        Div(attrs = {
+            style {
+                marginLeft(2.px)
+                marginTop(2.px)
+                borderRadius(40.px)
+                width(100.percent - 4.px)
+                height(100.percent - 4.px)
+                backgroundImage("url($backgroundImage)")
+                backgroundPosition("center")
+                backgroundSize("cover")
+                position(Position.Relative)
+                textAlign("center")
+            }
+        }) {
+            StatusBar()
+            Camera()
+        }
+    }
+}
