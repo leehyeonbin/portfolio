4dfd53ae3e6652cdf6dba495bf39101b239c5483
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-03-18 14:21:39 +0900

    💄️️:: body 에 style 지정

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,10 +1,12 @@
+import org.jetbrains.compose.web.css.Style
 import org.jetbrains.compose.web.renderComposable
 import screen.main.MainScreen
 import screen.main.PhoneScreen
+import ui.style.BodyStyleSheet
 
 fun main() {
-
     renderComposable(rootElementId = "root") {
+        Style(BodyStyleSheet)
         MainScreen()
         PhoneScreen()
     }
