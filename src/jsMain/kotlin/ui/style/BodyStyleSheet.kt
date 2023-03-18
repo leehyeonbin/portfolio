d204fcd7520dceb3d7e3978fb4fb39489cd91123
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-03-18 13:52:20 +0900

    ✨️️:: BodyStyleSheet 생성

---
+++ src/jsMain/kotlin/ui/style/BodyStyleSheet.kt
@@ -0,0 +1,14 @@
+package ui.style
+
+import org.jetbrains.compose.web.css.Color
+import org.jetbrains.compose.web.css.StyleSheet
+import org.jetbrains.compose.web.css.backgroundColor
+
+object BodyStyleSheet : StyleSheet() {
+    init {
+        "body" style {
+                    backgroundColor(Color.black)
+                }
+    }
+
+}
