f4e54a93c4660f020f75cea89ffc234dd490c222
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-03-25 22:29:28 +0900

    ⚰️️️️:: 사용하지 않는 code 제거

--- src/jsMain/kotlin/ui/style/AppIconStyleSheet.kt
+++ src/jsMain/kotlin/ui/style/AppIconStyleSheet.kt
@@ -3,11 +3,9 @@ package ui.style
 import org.jetbrains.compose.web.css.StyleSheet
 import org.jetbrains.compose.web.css.lineHeight
 import org.jetbrains.compose.web.css.px
-import org.jetbrains.compose.web.css.textAlign
 
 object AppIconStyleSheet : StyleSheet() {
     val container by style {
-        textAlign("center")
         lineHeight(0.px)
         property("filter", "brightness(1)")
         hover {
