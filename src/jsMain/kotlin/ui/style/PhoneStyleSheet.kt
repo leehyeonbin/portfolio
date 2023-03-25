37c98c5363ce51a3fdaba0e5b1325962607b899b
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-03-25 22:30:42 +0900

    ✨️️️️:: Phone Animation 생성

---
+++ src/jsMain/kotlin/ui/style/PhoneStyleSheet.kt
@@ -0,0 +1,26 @@
+package ui.style
+
+import org.jetbrains.compose.web.css.*
+
+object PhoneStyleSheet : StyleSheet() {
+    val animation by keyframes {
+        from {
+            property("transform", "translateY(-30%)")
+        }
+
+        to {
+            property("transform", "translateY(0%)")
+        }
+    }
+    val container by style {
+        textAlign("center")
+        lineHeight(0.px)
+        position(Position.Absolute)
+        right(0.px)
+        animation(animation) {
+            duration(1.s)
+            timingFunction(AnimationTimingFunction.EaseIn)
+//            direction(AnimationDirection.Alternate)
+        }
+    }
+}
