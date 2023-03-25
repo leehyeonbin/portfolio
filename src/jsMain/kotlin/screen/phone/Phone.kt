e11099fc0e609add2b5d2cbba8aace49700e1a4e
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-03-25 22:30:16 +0900

    💄️️️️:: style 변경

--- src/jsMain/kotlin/screen/phone/Phone.kt
+++ src/jsMain/kotlin/screen/phone/Phone.kt
@@ -1,35 +1,16 @@
 package screen.phone
 
 import androidx.compose.runtime.Composable
-import org.jetbrains.compose.web.ExperimentalComposeWebApi
-import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.css.Style
 import org.jetbrains.compose.web.dom.Div
 import screen.phone.device.S23
+import ui.style.PhoneStyleSheet
 
-@OptIn(ExperimentalComposeWebApi::class)
 @Composable
 fun Phone() {
+    Style(PhoneStyleSheet)
     Div (attrs = {
-        style {
-            animation(keyframesName = "phone-move", builder = {
-                duration(1.s)
-            })
-            position(Position.Absolute)
-            right(0.px)
-            CSSKeyframesBuilder {
-                from {
-                    transform {
-                        translateY((-120).percent)
-                    }
-                }
-                to {
-                    transform {
-                        translateY(0.percent)
-                    }
-                }
-            }
-//            buildKeyframes(name = "phone-move", builder = )
-        }
+        classes(PhoneStyleSheet.container)
     }){
         S23()
     }
