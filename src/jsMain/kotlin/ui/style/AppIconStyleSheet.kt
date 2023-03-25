6d3bc21939fda721e19d836319a947dd8d16107f
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-03-25 22:20:40 +0900

    ⚰️️️️:: 사용하지 않는 import 제거

--- src/jsMain/kotlin/ui/style/AppIconStyleSheet.kt
+++ src/jsMain/kotlin/ui/style/AppIconStyleSheet.kt
@@ -1,8 +1,11 @@
 package ui.style
 
-import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.css.StyleSheet
+import org.jetbrains.compose.web.css.lineHeight
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.textAlign
 
-object AppIconStyleSheet: StyleSheet() {
+object AppIconStyleSheet : StyleSheet() {
     val container by style {
         textAlign("center")
         lineHeight(0.px)
