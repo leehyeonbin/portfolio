8d375ef3e080f55cf5946e3369dbdfdd72200f25
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-03-26 13:37:01 +0900

    📄️️️️:: BlueToothModal 생성

--- src/jsMain/kotlin/screen/phone/display/bluetooth/BlueToothModal.kt
+++ src/jsMain/kotlin/screen/phone/display/bluetooth/BlueToothModal.kt
@@ -1,13 +1,18 @@
 package screen.phone.display.bluetooth
 
 import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.css.Style
 import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Text
+import ui.style.BlueToothStyleSheet
 
 @Composable
 fun BlueToothModal() {
+    Style(BlueToothStyleSheet)
     Div(attrs = {
-
+        classes(BlueToothStyleSheet.container)
     }) {
 
+        Text("asdasdfadfasdfasdff")
     }
 }
